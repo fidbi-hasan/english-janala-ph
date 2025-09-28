6a556641e6092fdcfbac61638bf386e1dc31a4f9
fidbi-hasan <fidbihasan21@gmail.com>
2025-09-28 22:35:01 +0600

    padding top for body fixed for mobile again

--- scripts/app.js
+++ scripts/app.js
@@ -32,7 +32,7 @@ const login = () => {
     icon: "success"
   });
 
-  document.getElementById('body').classList.add('pt-40');
+  document.getElementById('body').classList.add('pt-[500px]');
 
   heroSection.classList.add('hidden');
   navBar.classList.remove('hidden');
